b53ff2ce2ae80cb3c2de0630a632c786d30a6889
Zach Leatherman <zachleatherman@gmail.com>
2018-12-21 13:55:03 -0600

    Adds info, default run script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function() {
-	console.log( "Hi, I’m @zachleat." );
+	console.log( `           Hi, I’m Zach.
+
+      Web: https://zachleat.com/
+  Twitter: https://twitter.com/zachleat
+   GitHub: https://github.com/zachleat
+` );
 };
